b52a5ef48c6ca95256d6bb16492c73a3ea17d101
Diego Iván <diegoivan.mae@gmail.com>
2024-08-04 00:10:16 -0600

    parser: Add ParserError error domain

--- labelswap-data/src/parser.rs
+++ labelswap-data/src/parser.rs
@@ -4,3 +4,19 @@ pub mod yolo5obb_parser;
 
 pub use format_parser::FormatParser;
 pub use yolo5obb_parser::Yolo5ObbParser;
+
+use crate::models::format::SourceType;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ParserError {
+    #[error("Wrong file type: Expected: {expected}, found {found}")]
+    WrongFileType { expected: String, found: String },
+    #[error("Wrong destination, expected {expected}, but got {found}")]
+    WrongSource {
+        expected: SourceType,
+        found: SourceType,
+    },
+    #[error("Wrong format: {msg}")]
+    WrongFormat { msg: String },
+}
