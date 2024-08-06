74ba48dd6ebee45b8ff37dee2bec4c3c3bb6be3c
Diego Iván M.E <diegoivan.mae@gmail.com>
2024-08-06 15:47:34 -0600

    lib: Change resolve_relative_path function to use AsRef<Path>

--- labelswap-data/src/lib.rs
+++ labelswap-data/src/lib.rs
@@ -11,7 +11,10 @@ pub mod parser;
 pub mod serializer;
 pub mod transforms;
 
-pub fn resolve_relative_path(path: &Path, relative_path: &Path) -> Result<PathBuf, io::Error> {
+pub fn resolve_relative_path<P, R>(path: P, relative_path: R)-> Result<PathBuf, io::Error>
+where P: AsRef<Path>,
+      R: AsRef<Path> {
+    let path: &Path = path.as_ref();
     let canonical_path = path.canonicalize()?;
     Ok(canonical_path.join(relative_path))
 }
