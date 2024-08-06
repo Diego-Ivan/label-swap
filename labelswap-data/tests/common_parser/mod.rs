8a12cbfb9aecae47e464ede48638cfb69537113c
Diego Iván M.E <diegoivan.mae@gmail.com>
2024-08-06 15:34:46 -0600

    common-parser: Add resolve_test_path function

--- labelswap-data/tests/common_parser/mod.rs
+++ labelswap-data/tests/common_parser/mod.rs
@@ -1,5 +1,7 @@
 use labelswap_data::{models::annotation::Annotation, *};
 
+use std::path::{Path, PathBuf};
+
 pub fn test_annotation(parser: &mut impl parser::FormatParser, expected: Vec<Annotation>) {
     let mut annotations = Vec::new();
     while parser.has_next() {
@@ -9,4 +11,10 @@ pub fn test_annotation(parser: &mut impl parser::FormatParser, expected: Vec<Ann
         }
     }
     assert_eq!(annotations, expected);
+}
+
+pub fn resolve_test_path(path: impl AsRef<Path>) -> PathBuf {
+    let mut cwd = std::env::current_dir().expect("Could not get current directory");
+    cwd.push(path);
+    return cwd;
 }
